f0fcfbe75fa7339dff95e9fff11f825b664fa7c7
Danielkognita <daniel.goncalves@kognita.ai>
2025-12-23 21:25:06 -0300

    fix

--- components/PropertyForm.tsx
+++ components/PropertyForm.tsx
@@ -246,7 +246,8 @@ const VehicleForm: React.FC<VehicleFormProps> = ({ onSubmit, isLoading, defaultU
           </div>
         </div>
 
-         <button
+       
+        <button
           type="submit"
           disabled={isLoading}
           className={`w-full py-5 rounded-2xl font-black text-white shadow-xl transition-all transform active:scale-[0.98] uppercase tracking-widest text-xs flex items-center justify-center gap-3 ${
@@ -267,6 +268,7 @@ const VehicleForm: React.FC<VehicleFormProps> = ({ onSubmit, isLoading, defaultU
             <>AVALIAR AGORA <ArrowRight className="w-4 h-4" /></>
           )}
         </button>
+      
       </form>
     </div>
   );
